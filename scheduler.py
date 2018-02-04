c11d99a80952d357992808e99bdc2cb5f9eda097
Danny Tran <danny.qa.tran@gmail.com>
2018-02-03 23:16:13 -0800

    Scheduler.py seems to be working

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,8 @@
 import itertools
 from mainCourseClass import MainCourse
 from courseClass import Course
+from collections import defaultdict
+
 """
 Scheduler generates a list of all the possible combinations of class
 the user can take
@@ -12,7 +14,7 @@ class Scheduler:
 		format of badTimes: {Day: [(start, end)]}
 		"""
 		self.scheduleCombinations = []
-		self.takenTimes = dict() 
+		self.takenTimes = defaultdict(list)
 		self._fillInBadTimes(badTimes)
 		self._scheduler(allCourses)
 
@@ -59,7 +61,12 @@ class Scheduler:
 					continue
 				break
 			if not conflict:
-				self.scheduleCombinations.append(combined)
+				# self.scheduleCombinations.append(combined)
+				# uncomment top and delte this, for testing
+				l = []
+				for i in combined:
+					l.append(i.getCourseCode())
+				self.scheduleCombinations.append(l)
 
 		# for combo in permutationDiffCourses:
 		# 	takenTimesPerCombo = self.takeTimes.copy()
@@ -108,10 +115,19 @@ class Scheduler:
 
 if __name__ == "__main__":
 	Lab311 = Course(16041, "Lab", "A1", "HEYDARIGORJI, A.", "M   8:00-10:50", "ELH 110")
+	print(Lab311)
 	Lab312 = Course(16042, "Lab", "A2", "TAZARV, A.", "Tu   11:00- 1:50p0", "ELH 110")
 	lab31list = [Lab311, Lab312]
 	Lec31L = MainCourse(16040, "Lec", "A", "DANG, Q.", "Tu   5:00- 6:50p", "SSH 100", lab31list)
-	allCourses = {"CSE 31L": [Lec31L]}
+
+
+	Lab331 = Course(36130, "Lab", "10", "LIU, J.", "TuTh   2:00- 3:50p", "ELH 110")
+	Lab332 = Course(36131, "Lab", "10", "LIU, J.", "TuTh   2:00- 3:50p", "ELH 110")
+	Lab333 = Course(36133, "Lab", "10", "LIU, J.", "TuTh   2:00- 3:50p", "ELH 110")
+	Lab33List = [Lab331, Lab332, Lab333]
+	Lec33a = MainCourse(36110, "Lec", "A", "DANG, Q.", "MWF   1:00- 1:50p", "SSH 100", Lab33List)
+	Lec33b = MainCourse(36115, "Lec", "B", "DANG, Q.", "MWF   4:00- 4:50p", "SSH 100", Lab33List)
+	allCourses = {"CSE 31L": [Lec31L], "ICS 33": [Lec33a, Lec33b]}
 
 	x = Scheduler(allCourses, {})
 	print(x.getScheduleCombinations())
