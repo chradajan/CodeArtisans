814f24d971864c7275555f76d823aadd0046129d
Christopher Janzen <chradajan@gmail.com>
2018-02-03 22:29:38 -0800

    Added regex for all parameters

--- scraper.py
+++ scraper.py
@@ -4,10 +4,10 @@ from html.parser import HTMLParser
 from courseClass import Course
 from mainCourseClass import MainCourse
 import re
-from flask import flask
-app = Flask(__name__)
+#from flask import flask
+#app = Flask(__name__)
 
-@app.route("/courseread")
+#@app.route("/courseread")
 def CourseReader(Dept: str, CourseNum: str):# -> [MainCourse]
 	mainCourseList = []
 	link = "https://www.reg.uci.edu/perl/WebSoc?YearTerm=2018-03&ShowFinals=1&ShowComments=0&Dept={}&CourseNum={}".format(Dept, CourseNum)
@@ -17,17 +17,56 @@ def CourseReader(Dept: str, CourseNum: str):# -> [MainCourse]
 			decodedLine = line.decode()
 			if 'Bookstore' in decodedLine:
 				courseCode = units = 0
-				courseType = courseType = courseSection = instructor = time = place = ""
+				courseType = courseType = courseSection = instructor = time = place = final = ""
 
+				#Parse course number
 				match = re.search('\d\d\d\d\d', decodedLine)
 				if match:
-					courseNumber = match.group()
+					courseCode = int(match.group())
 
-				match = re.search('Lec|Dis|Lab', decodedLine)
+				#Parse type
+				match = re.search('Lec|Dis|Lab|Tap', decodedLine)
 				if match:
 					courseType = match.group()
-				print(courseNumber + "  " + courseType)
+
+				#Parse Section
+				match = re.search('>((\w\w?)|(\d\d?))<', decodedLine)
+				if match:
+					courseSection = match.groups()[0].replace('>', '').replace('<', '')
+
+				#Parse units
+				match = re.search('>\d<', decodedLine)
+				if match:
+					units = int(match.group().replace('>', '').replace('<', ''))
+
+				#Parse instructor
+				match = re.findall('>((?:\w+(?: ?\w+)?, \w+.)|(?:STAFF))<', decodedLine)
+				if match:
+					instructor = '  '.join(match)
+
+				#Parse Time
+				match = re.search('\w+[ ]+&nbsp;[ ]+\d+:\d+-[ ]?\d+:\d+p?', decodedLine)
+				if match:
+					time = match.group().replace(' &nbsp;', '')
+
+				#Parse Place
+				match = re.search('>\w+ \d+[A-Z]*<', decodedLine)
+				if match:
+					place = match.group().replace('>', '').replace('<', '')
+
+				#Parse Final
+				match = re.search('\w+, +\w+ +\d+, +\d+:\d+-\d+:\d+p?m?', decodedLine)
+				if match:
+					final = match.group()
+				elif units > 0:
+					final = 'TBA'
+
+				if units > 0:
+					mainCourseList.append()
+				
+
+				#print('{:5}  {:3}  {:2}  {:1}  {:20}  {:17}  {:10}  {}'.format(courseCode, courseType, courseSection, units, instructor, time, place, final))
 
 
 if __name__ == '__main__':
-	CourseReader('CSE', '31L')
+	CourseReader('Physics', '7C')
