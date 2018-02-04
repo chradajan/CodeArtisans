79a5d2f8f0821d4b3d3a15b8df574a069c2ac30d
Christopher Janzen <chradajan@gmail.com>
2018-02-03 22:31:53 -0800

    Co courses now added to mainCourse via function

--- mainCourseClass.py
+++ mainCourseClass.py
@@ -1,9 +1,12 @@
 from courseClass import Course
 
 class MainCourse(Course):
-	def __init__(self, courseCode: int, courseType: str, courseSection: str, instructor: str, time: str, place: str, coCourse: list):
+	def __init__(self, courseCode: int, courseType: str, courseSection: str, instructor: str, time: str, place: str):
 		super().__init__(courseCode, courseType, courseSection, instructor, time, place)
-		self.coCourse = coCourse
+		self.coCourse = []
+
+	def addCoCourse(newCoCourse: Course):
+		self.coCourse.append(newCoCourse)
 
 	def getCoCourses(self):
 		return self.coCourse
